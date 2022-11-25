c6f309d0de776b8b8ecddb0452389759ba19b133
Zapadynsky <e.zapadinsky@gmail.com>
2022-11-25 09:27:52 +0200

    modify2 create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -6,6 +6,6 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 storage = MemoryStorage()
 
 
-bot = Bot(token=config.TOKEN)
+bot = Bot(token=config.TOKEN) # using for heroku
 # bot = Bot(token=os.getenv('TOKEN')) # using for localhost
 dp = Dispatcher(bot, storage=storage)
